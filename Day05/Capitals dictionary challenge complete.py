ef97e6a8fba6a1d3b740206fc0b89c37d76a3efb
Holiday <jon.holiday1@gmail.com>
2026-04-12 01:44:02 -0700

    Day 05 - Capitals dictionary challenge complete

---
+++ Day05/Capitals dictionary challenge complete.py
@@ -0,0 +1,9 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+}
+
+
+for country, capital in capitals.items():
+    print(f"The capital of {country} is {capital}.")
